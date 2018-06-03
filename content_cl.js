3f7583db83fef6d1d0624068801429155690be36
Misuzi <misuzichan@gmail.com>
2018-06-03 00:38:48 -0500

    Click List Content Implemented

---
+++ content_cl.js
@@ -0,0 +1,28 @@
+// content_cl.js
+// Click List Script
+
+function interact()
+{
+  var name = document.getElementsByClassName("headline1");
+
+  if (name[0].innerHTML == "EGG")
+  {
+    button = document.getElementsByClassName("warmButton");
+    button[0].getElementsByTagName("input")[0].click();
+  }
+  else
+  {
+    var button = document.getElementsByClassName("train_button");
+    button[0].click();
+  }
+}
+
+(function loop()
+{
+  var delay = 250 + (Math.floor((Math.random() * 250) + 1));
+  setTimeout(function()
+    {
+      interact();
+      loop();
+    }, delay);
+}());
